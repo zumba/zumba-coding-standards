42cf33a52aca7ed6bf4b413cd81df8873bab3a2d
David Meybohm <david.meybohm@zumba.com>
2018-01-07 14:40:51 -0500

    Fix test

--- test/data/use-in-trait.php
+++ test/data/use-in-trait.php
@@ -12,8 +12,7 @@ trait Foo {
 --EXPECT--
 
 --------------------------------------------------------------------------------
-FOUND 2 ERROR(S) AFFECTING 2 LINE(S)
+FOUND 1 ERROR(S) AFFECTING 1 LINE(S)
 --------------------------------------------------------------------------------
  6 | ERROR | The "use" has been used before on line 5
- 9 | ERROR | The "use" has been used before on line 5
 --------------------------------------------------------------------------------
